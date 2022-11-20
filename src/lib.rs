e2a6905a0d88751370b966475c5fd4033aa02ff3
rrohrer <ryan.rohrer@gmail.com>
2022-11-20 02:43:09 -0800

    Add comments so the code makes sense.

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,22 @@
 use std::collections::HashMap;
 
+// Import the GL functions that are used to make this demo work.
 extern "C" {
     fn glViewport(x: i32, y: i32, width: i32, height: i32);
     fn glClearColor(r: f32, g: f32, b: f32, a: f32);
     fn glClear(bit_mask: u32);
 }
 
+/// Initialize a "game engine"
+/// This function only gets called once.
 #[no_mangle]
 extern "C" fn init_engine() {
     let v = vec![0];
     unsafe { glViewport(v[0], 0, 280, 210) }
 }
 
+/// Pretend to update a game.
+/// This gets called at the beginning of every animation frame.
 #[no_mangle]
 extern "C" fn update_game() {
     let a = vec![1, 0, 0, 1, 3];
@@ -21,6 +26,8 @@ extern "C" fn update_game() {
     }
 }
 
+/// Pretend to update graphics
+/// This gets called at the end of every animation frame.
 #[no_mangle]
 extern "C" fn update_renderer() {
     let h = {
